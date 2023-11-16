095cf0f9c642c24586b51a251c2886b4c1743bfb
Raj Rathod <rajrasikrathod@gmail.com>
2023-11-16 14:11:13 +1300

    part 1 of hw 8 complete

--- homework8/main.py
+++ homework8/main.py
@@ -1,6 +1,6 @@
 from dto import InventoryItem, ItemOrigin
-from fastapi import FastAPI
-from typing import Dict
+from fastapi import FastAPI, HTTPException
+from typing import*
 
 app = FastAPI() # when calling FastAPI what is it doing
 
@@ -8,10 +8,26 @@ my_inventory_item_dict:Dict[str, InventoryItem] = {} # currently empty dict, use
 # used to look up inventory item using the serial number
 
 @app.put("/items/{serial_num}") #naming convention inside curly braces needs to be the same as everything else in parameters for the function, the original class creation and the path
-def create_item(item:InventoryItem, serial_num:str):
+def create_item(item:InventoryItem, serial_num:str) -> None:
     my_inventory_item_dict[serial_num] = item
     print(my_inventory_item_dict)
 
 @app.get("/items/{serial_num}") #naming convention inside curly braces needs to be the same as everything else in parameters for the function, the original class creation and the path
-def read_item(serial_num: str): #union[str, none] means the type can be str or none, but the = none means default value is none, so you don't have to specify it
-    return {"item":my_inventory_item_dict[serial_num]}
+def read_item(serial_num: str) -> InventoryItem: #union[str, none] means the type can be str or none, but the = none means default value is none, so you don't have to specify it
+    if serial_num in my_inventory_item_dict.keys():
+        return {"item":my_inventory_item_dict[serial_num]}
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+@app.delete("/items/{serial_num}")
+def delete_item(serial_num:str) -> Dict:
+      if serial_num in my_inventory_item_dict.keys():
+        my_deleted_item = my_inventory_item_dict.pop(serial_num)
+        print(my_inventory_item_dict)
+        return {"msg": "item {my_deleted_item} is successfully deleted"}
+      else:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+@app.get("/items/") 
+def get_item() -> List[InventoryItem]: # -> List[InventoryItem] I've specified what this function is returning
+    return my_inventory_item_dict.values() # getting a list of values
