97651d96bfefd00b1188e7eca0cab54a2f46d65a
Raj Rathod <rajrasikrathod@gmail.com>
2023-12-01 17:43:45 +1300

    python_sql_homework updated

--- python_sql_homework/main.py
+++ python_sql_homework/main.py
@@ -1,7 +1,7 @@
 from typing import Optional, List
 from contextlib import asynccontextmanager
 # One line of FastAPI imports here later &#x1f448;
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import Field, Session, SQLModel, create_engine, select, delete
 
 
@@ -35,15 +35,28 @@ def create_hero(hero: Hero) -> None: # why do we
         session.add(hero)
         session.commit()
 
+# new way to do DTO, it defines the payload when you want to have an update operation
+class HeroUpdate(SQLModel): # specialized class for each database operation Create Read Update Delete. 
+    name:Optional[str] = None
+    secret_name: Optional[str] = None
+    age:Optional[int] = None
 
-@app.get("/heroes/{name}")
+@app.patch("/hero/", response_model=Hero) # if you want to change more than one piece of data is by create and customize a hero model. The way to do it is create another sql model, and it's only used to change existing the DAO in the database
+def change_secret_name(hero_update:HeroUpdate):
+    with Session(engine) as session:
+        db_hero = session.get(Hero, hero_update.name).first() #.one() can be used but that is a syntax from sqlalchemy
+        if not db_hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+    
+
+@app.get("/heroes/{name}", response_model=Hero)
 def get_hero(name: str) -> Hero:
     with Session(engine) as session:    
         statement = select(Hero).where(Hero.name == name)
         retrieved_hero = session.exec(statement).first() # .all() does not work if there is only one hero called 'batman' for e.g. in the db, in that scenario use first        
     return retrieved_hero
 
-@app.get("/heroes/") # response=List[Hero]
+@app.get("/heroes/", response_model=List[Hero])
 def get_heroes() -> List[Hero]:
     with Session(engine) as session:  
         statement = select(Hero).where()
@@ -56,3 +69,6 @@ def delete_hero(id):
         statement = delete(Hero).where(Hero.id == id)
         session.exec(statement)
         session.commit()
+
+#pydantic usually automatically does the typehinting for the output of our function, but since we're not using pydantic here
+# we need to specify the response
